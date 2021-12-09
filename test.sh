485ae976ec975e5345a8d3c787ffee6b91fdf1a2
asarvazyan <areg.sarvazyan@gmail.com>
2021-12-09 21:57:35 +0100

    fin parte 2

--- test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-touch test.output
-./cmc -v tmp/a00.c > test.output
-./cmc -v tmp/a01.c >> test.output
-./cmc -v tmp/a02.c >> test.output
-./cmc -v tmp/a03.c >> test.output
-./cmc -v tmp/a04.c >> test.output
